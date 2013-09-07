62c13b2527537070f8123ce6f8ebc51619ac79c1
Steven G. Harms <steven.harms@gmail.com>
2013-09-07 02:01:40 -0700

    Test for DefaultTranslator

--- test/default_translator_test.rb
+++ test/default_translator_test.rb
@@ -9,27 +9,29 @@ class DefaultTranslatorTest < Minitest::Unit::TestCase
   def test_basic
     stub_table = OpenStruct.new({
       T: OpenStruct.new({
-      subject: 'Stub T',
-      verb: 'Stub T',
-      implement: 'Stub T',
-      locus: 'Stub T'
-    }),
+        subject: 'Stub T',
+        verb: 'Stub T',
+        implement: 'Stub T',
+        locus: 'Stub T'
+      }),
       E: OpenStruct.new({
-      subject: 'Stub E',
-      verb: 'Stub E',
-      implement: 'Stub E',
-      locus: 'Stub E'
-    }),
+        subject: 'Stub E',
+        verb: 'Stub E',
+        implement: 'Stub E',
+        locus: 'Stub E'
+      }),
       S: OpenStruct.new({
-      subject: 'Stub S',
-      verb: 'Stub S',
-      implement: 'Stub S',
-      locus: 'Stub S'
-    }),
+        subject: 'Stub S',
+        verb: 'Stub S',
+        implement: 'Stub S',
+        locus: 'Stub S'
+      }),
     })
     stub_offsetter = ::RotasNullOffsetter.new
     translator =  RotasDefaultTranslator.new(stub_table, stub_offsetter)
-    translator.translate("test".split(''))
+    result = translator.translate("test".split(''))
+    assert_equal 4, result.length, "Should return array with 4 elements"
+    assert result.first == result.last, "T and T should return the same translated result"
   end
 
 end
